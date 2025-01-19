8cc029478d968dd2a515714443543973e7245119
Nayan Mandal <121637927+mandalnayan@users.noreply.github.com>
2025-01-19 19:24:29 +0530

    Deployed on online server

--- smartcontactmanager/src/main/java/com/contact/smartcontactmanager/SmartcontactmanagerApplication.java
+++ smartcontactmanager/src/main/java/com/contact/smartcontactmanager/SmartcontactmanagerApplication.java
@@ -1,13 +1,15 @@
 package com.contact.smartcontactmanager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartcontactmanagerApplication {
-
+  @Value("${server.port}")
+  static String port;
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcontactmanagerApplication.class, args);
-		System.out.println("\n Spring boot is running on port 8282 !!");    
+		System.out.println("\n Spring boot is running on port " + port);    
 	}
 }
